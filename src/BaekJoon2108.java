b85781bb8e53345bf2bc9cfdb29d8522818abd91
hjseo <mijin2197@hanmail.net>
2022-12-28 16:52:27 +0900

    백준 통계학(산술평균, 중앙값, 최빈값, 범위 출력)

--- src/BaekJoon2108.java
+++ src/BaekJoon2108.java
@@ -1,83 +1,88 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
+/**
+ * 백준 통계학 (https://www.acmicpc.net/problem/2108)
+ * 
+ * 첫째 줄에는 산술평균을 출력한다. 소수점 이하 첫째 자리에서 반올림한 값을 출력한다.
+ * 둘째 줄에는 중앙값을 출력한다.
+ * 셋째 줄에는 최빈값을 출력한다. 여러 개 있을 때에는 최빈값 중 두 번째로 작은 값을 출력한다.
+ * 넷째 줄에는 범위를 출력한다.
+ *
+ */
 public class BaekJoon2108 {
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
 		
+		int n = Integer.parseInt(br.readLine());
 		int[] arr = new int[n];
-		int total = 0;
+		int[] plusArr = new int[4002];
+		int[] minusArr = new int[4001];
+		int sum = 0;
 		
 		for(int i=0; i<n; i++) {
 			arr[i] = Integer.parseInt(br.readLine());
-			total += arr[i];
+			if(arr[i] >= 0) {
+				plusArr[arr[i]]++;
+			} else {
+				minusArr[-arr[i]]++;
+			}
+			sum += arr[i];
 		}
 		
-		StringBuilder sb = new StringBuilder();
-		
-		//산술평균
-		int rst1 = (int)Math.round(((double)total /n));
-		sb.append(rst1).append('\n');
+		//1. 산술평균
+		sb.append((int)Math.round(((double)sum /n))).append('\n');
 		
 		Arrays.sort(arr);
 		
-		//중앙값
-		int rst2 = arr[n/2];
-		sb.append(rst2).append('\n');
+		//2. 중앙값
+		sb.append(arr[n/2]).append('\n');
 		
-		int[] plusArr = new int[4002];
-		int[] minusArr = new int[4001];
-
-		for(int i=0; i<n; i++) {
-			if(arr[i] < 0) {
-				//음수
-				minusArr[Math.abs(arr[i])]++;
-			} else {
-				plusArr[arr[i]]++;
-			}
-		}
+		//3. 최빈값
+		int max = Integer.MIN_VALUE;
+		List<Integer> list = new ArrayList<>();
 		
-		ArrayList<Integer> arrList = new ArrayList<>();
-		
-		int maxValue = 0;
 		for(int i=0; i<plusArr.length; i++) {
-			maxValue = Math.max(maxValue, plusArr[i]);
+			if(max < plusArr[i]) {
+				list.clear();
+				max = plusArr[i];
+				list.add(i);
+			} else if(max == plusArr[i]) {
+				list.add(i);
+			}
 		}
 		
 		for(int i=0; i<minusArr.length; i++) {
-			maxValue = Math.max(maxValue, minusArr[i]);
-		}
-		
-		for(int i=0; i<arr.length; i++) {
-			if(i<0) {
-				if(minusArr[Math.abs(i)] == maxValue) {
-					arrList.add(minusArr[i]);
-				}
-			} else {
-				if(plusArr[i] == maxValue) {
-					arrList.add(plusArr[i]);
-				}
+			if(max < minusArr[i]) {
+				list.clear();
+				max = minusArr[i];
+				list.add(-(i));
+			} else if(max == minusArr[i]) {
+				list.add(-(i));
 			}
 		}
 		
-		//최빈값
-		int rst3 = 0;
-		System.out.println("size:::   " + arrList.size());
-		if(arrList.size() > 1) {
-			rst3 = arrList.get(1);
+		if(list.size() == 1) {
+			sb.append(list.get(0)).append('\n');
+		} else if(list.size() > 1){
+			Collections.sort(list);
+			sb.append(list.get(1)).append('\n');
 		} else {
-			rst3 = arrList.get(0);
+			sb.append('\n');
 		}
 		
-		sb.append(rst3).append('\n');
-		
-		//범위
-		int rst4 = arr[n-1] - arr[0];
-		sb.append(rst4).append('\n');
+		//4. 범위
+		sb.append(arr[n-1] - arr[0]);
 		
 		System.out.println(sb);
+		
+		br.close();
+		
 	}
 }
