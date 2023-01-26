a3dbc62c78442e9378eca781fbf9f98daad31e8a
hjseo <mijin2197@hanmail.net>
2023-01-26 20:52:45 +0900

    자신이 맞은 문제 수, 아이디 출력

---
+++ src/BaekJoon7287.java
@@ -0,0 +1,13 @@
+
+/**
+ * 백준 등록 (https://www.acmicpc.net/problem/7287)
+ * 자신의 맞은 문제 수, 아이디 출력
+ * @author hodu
+ *
+ */
+public class BaekJoon7287 {
+	public static void main(String[] args) {
+		System.out.println(138);
+		System.out.println("mijin2197");
+	}
+}
