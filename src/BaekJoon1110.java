a24f833c800d6a01c778962064635f21eccdf42c
hjseo <mijin2197@hanmail.net>
2023-01-30 16:41:25 +0900

    N의 사이클을 구하여 출력 수정(해결)

--- src/BaekJoon1110.java
+++ src/BaekJoon1110.java
@@ -2,38 +2,43 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/**
+ * 백준 더하기 사이클 (https://www.acmicpc.net/problem/1110)
+ * N의 사이클을 구하여 출력
+ * @author hodu
+ *
+ */
 public class BaekJoon1110 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int newNum = n;
+		int count = 0;
 		
-		int[] inputNum = new int[2];
-		int num = Integer.parseInt(br.readLine());
-		int calNum = 0;
-		int calCnt = 0;
 		
-		if(num < 10) {
-			inputNum[0] = 0;
-			inputNum[1] = num;
-		} else {
-			inputNum[0] = (int) Integer.toString(num).charAt(0);
-			inputNum[1] = (int) Integer.toString(num).charAt(1);
-			System.out.println(Integer.toString(num).charAt(0));
-			System.out.println(Integer.toString(num).charAt(1));
-		}
-		
-//		while(true) {
-		for(int i=0; i<5; i++) {
-			calCnt++;
+		while(true) {
 			
-			calNum = (int) inputNum[0] + inputNum[1];
-			System.out.println(i + "   " + calNum);
-			inputNum[0] = inputNum[1];
-			inputNum[1] = Integer.toString(num).charAt(1);
-			
-			if(calNum == num) {
-				break;
+			if(count > 0) {
+				if(n == newNum) break;
 			}
+			
+			count++;
+			
+			if(newNum >= 10) {
+				int num1 = newNum / 10;	//십의자리 수
+				int num2 = newNum % 10;	//일의자리 수
+				
+				int sum = num1 + num2;
+				newNum = num2 * 10 + sum % 10;
+			} else {
+				newNum = newNum * 10 + newNum;
+			}
+			
 		}
-		System.out.println(calCnt);
+		
+		System.out.println(count);
+		
+		br.close();
+		
 	}
 }
